36193169f76486f870467bc20a4de2849f5a87f7
Bradman Davis <bradman.davis@my.jcu.edu.au>
2024-11-25 11:54:04 +1000

    Delete tRScript.sh

--- tRScript.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST \
-  -H 'Content-type: application/json' \
-  -H 'auth-token: 1abca0d7-f4aa-4135-8b43-e785c7dfbaa8' \
-  --data '{ "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
-  https://api.testrigor.com/api/v1/apps/Waj384CbetLwqoBhu/retest
